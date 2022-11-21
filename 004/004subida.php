0b38f07f5fb0b811920966de126c64714f5e92cc
neumanv <guillermoanv25@gmail.com>
2022-11-21 18:38:39 +0100

    Hecho

--- 004/004subida.php
+++ 004/004subida.php
@@ -1,25 +1,23 @@
 <?php
 //array asociativo de elementos
-if(isset($_FILES['image'])){
+if (isset($_FILES['image'])) {
     $errors = array();
-    $file_name = $_FILES['image']['name'];  //nombre del archivo
-    $file_size = $_FILES['image']['size'];  //tamaño del archivo
+    $file_name = $_FILES['image']['name'];
+    $file_size = $_FILES['image']['size'];
     $file_tmp = $_FILES['image']['tmp_name'];
     $file_type = $_FILES['image']['type'];
     $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
     $extensions = array("jpeg", "jpg", "png");
-
-    if (in_array($file_ext, $extensions) === false){
+    if (in_array($file_ext, $extensions) == false) {
         $errors[] = "Estensión no permitida. Prueba con imágenes jpeg,jpng o png.";
     }
-
-    if ($file_size > 2097152){
+    if ($file_size > 2097152) {
         $errors[] = 'El archivo no puede pesar más de 2MB';
     }
-    if (empty($errors) == true){
-        move_uploaded_file($file_tmp, "../images/" . $file_name);
+    if (empty($errors) == true) {
         echo "La imagen se ha guardado correctamente";
     } else {
         print_r($errors);
     }
 }
+?>
