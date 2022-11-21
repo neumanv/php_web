2420598254ec27b420d75ff72df03e66faf360c1
neumanv <guillermoanv25@gmail.com>
2022-11-21 11:05:31 +0100

    Hecho

--- 001/001server.php
+++ 001/001server.php
@@ -1,15 +1,14 @@
 <?php
 
-echo $_SERVER["PHP_SELF"]."<br>"; 
-echo $_SERVER["SERVER_SOFTWARE"]."<br>"; 
-echo $_SERVER["SERVER_NAME"]."<br>";
-echo $_SERVER["REQUEST_METHOD"]."<br>"; 
-echo $_SERVER["REQUEST_URI"]."<br>";
-echo $_SERVER["QUERY_STRING"]."<br>";
+echo $_SERVER["PHP_SELF"]."<br>"; //El nombre del archivo de script ejecutándose actualmente
+echo $_SERVER["SERVER_SOFTWARE"]."<br>"; //Cadena de identificación del servidor dada en las cabeceras de respuesta a las peticiones.
+echo $_SERVER["SERVER_NAME"]."<br>"; //El nombre del host del servidor donde se está ejecutando actualmente el script
+echo $_SERVER["REQUEST_METHOD"]."<br>"; //Método de petición empleado para acceder a la página
+echo $_SERVER["REQUEST_URI"]."<br>"; //La URI que se empleó para acceder a la página
+echo $_SERVER["QUERY_STRING"]."<br>"; //Si existe, la cadena de la consulta de la petición de la página.
+echo $_SERVER["HTTP_USER_AGENT"]."<br>"; //Consiste en una cadena que indica el agente de usuario empleado para acceder a la pagina
 
-echo $_SERVER["HTTP_USER_AGENT"]."<br>";
-
-$invalida = $_GET[""];
+$invalida = $_GET[""]; //Da error porque el formulario no recibe parametros con get
 
 $nombre = $_POST["nombre"];
 $apellidos = $_POST["apellidos"];
@@ -18,8 +17,9 @@ $contrasena = $_POST["contrasena"];
 
 //----- MOSTRAR -----------------------------
 
-echo $nombre;
-echo $apellidos;
-echo $user;
-echo $contrasena;
+echo "<br>";
+echo "Nombre: ".$nombre."<br>";
+echo "Apellidos: ".$apellidos."<br>";
+echo "Nombre usuario: ".$user."<br>";
+echo "Contraseña: ".$contrasena;
 ?>
