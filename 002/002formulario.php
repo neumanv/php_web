0ef83f91dbc0a70e98e4c34f0aafffd9ec4ee74f
neumanv <guillermoanv25@gmail.com>
2022-11-21 12:23:05 +0100

    Cambios

---
+++ 002/002formulario.php
@@ -0,0 +1,12 @@
+<?php
+
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellidos"];
+$email = $_POST["email"];
+$sexo[] = $_POST["sexo[]"];
+$convivientes = $_POST["mate"];
+$hobby[] = $_POST["hobby[]"];
+$favorito = $_POST["favorito"];
+
+echo $favorito ,$hobby[1]; 
+?>
