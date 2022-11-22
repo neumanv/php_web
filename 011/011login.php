f1c11b8ba13f061b555e3b0178ba4cda1c326dd0
neumanv <guillermoanv25@gmail.com>
2022-11-22 12:08:30 +0100

    Cambios

--- 011/011login.php
+++ 011/011login.php
@@ -6,17 +6,25 @@ $password = $_POST["password"];
 
 $user =  strtolower($user);
 
-if(strlen($user) < 7){
-    echo "El nombre de usuario es demasiado pequeño(mínimo 7 cifras).";
-}else if(strlen($user) > 15){
-    echo "El nombre de usuario es demasiado grande(máximo 15 cifras).";
-}else if(preg_match("/[\W]/", $user)){
-    echo "El nombre sólo puede incluir números y letras.";
-}else{
+if(isset($user) && isset($password)) {
+    if(strlen($user) < 7){
+        echo "El nombre de usuario es demasiado pequeño(mínimo 7 cifras).";
+    }else if(strlen($user) > 15){
+        echo "El nombre de usuario es demasiado grande(máximo 15 cifras).";
+    }else if(preg_match("/[\W]/", $user)){
+        echo "El nombre sólo puede incluir números y letras.";
+    }
     
+    if(strlen($password) < 7){
+        echo "La contraseña no es segura";
+    }else if(strlen($password) > 30){
+        echo "La contraseña es demasiado larga.";
+    }    
+}else{
+    echo "Campos incompletos";
 }
 
-//Contraseña -----------------------------------------
+//Abrir sesión -----------------------------------------
 
-
+header("Location: http://www.example.com/");
 ?>
