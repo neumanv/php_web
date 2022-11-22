cac5e421ac5d6e6536b1ccda5dd7a4180adacfbd
neumanv <guillermoanv25@gmail.com>
2022-11-22 12:42:07 +0100

    Hecho

--- 011/011login.php
+++ 011/011login.php
@@ -6,25 +6,19 @@ $password = $_POST["password"];
 
 $user =  strtolower($user);
 
-if(isset($user) && isset($password)) {
+
     if(strlen($user) < 7){
         echo "El nombre de usuario es demasiado pequeño(mínimo 7 cifras).";
     }else if(strlen($user) > 15){
         echo "El nombre de usuario es demasiado grande(máximo 15 cifras).";
     }else if(preg_match("/[\W]/", $user)){
-        echo "El nombre sólo puede incluir números y letras.";
-    }
-    
-    if(strlen($password) < 7){
+        echo "El nombre sólo puede incluir números, letras y guiones bajos.";
+    }elseif(strlen($password) < 7){
         echo "La contraseña no es segura";
     }else if(strlen($password) > 30){
         echo "La contraseña es demasiado larga.";
-    }    
-}else{
-    echo "Campos incompletos";
-}
-
-//Abrir sesión -----------------------------------------
-
-header("Location: http://www.example.com/");
+    }else{
+        //Abrir sesión 
+        header('Location: http://localhost/php_web/012/012peliculas.php');
+    }
 ?>
---
+++ 012/012peliculas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Act 12</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/custom.js"></script>
+</head>
+<body>
+    <h3 class="card-title text-center">LISTADO DE PELÍCULAS</h3>
+</body>
+</html>
